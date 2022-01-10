7e1390eaf2fc72185c25689619a5e9c2e5fbd71c
Mikhail Pyatykh <tryingstuff@yandex.ru>
2022-01-10 18:08:35 +0900

    final

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,13 @@ const buttonEditProfile = document.querySelector('.profile__edit-button');
 const buttonCloseEditProfile = document.querySelector('.popup__close-btn');
 const popup = document.querySelector('.popup');
 const popupOpenedClass = 'popup_opened';
+let nameProfile = document.querySelector('#name');
+let jobProfile = document.querySelector('#job');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.input__info-fio');
+let jobInput = formElement.querySelector('.input__info-occupation');
+nameInput.value = nameProfile.textContent;
+jobInput.value = jobProfile.textContent;
 
 buttonEditProfile.addEventListener('click', function() {
     popup.classList.add(popupOpenedClass);
@@ -9,4 +16,25 @@ buttonEditProfile.addEventListener('click', function() {
 
 buttonCloseEditProfile.addEventListener('click', function() {
   popup.classList.remove(popupOpenedClass);
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 });
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let emptyString = '';
+  nameInput.textContent = nameProfile.textContent;
+  jobInput.textContent = jobProfile.textContent;
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  if (nameInput.value == emptyString) {
+    nameProfile.textContent = nameInput.textContent;
+  }
+  if (jobInput.value == emptyString) {
+    jobProfile.textContent = jobInput.textContent;
+  }
+  popup.classList.remove(popupOpenedClass);
+  }
+
+
+formElement.addEventListener('submit', formSubmitHandler);
